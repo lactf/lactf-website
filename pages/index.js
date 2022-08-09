e9cef22f109932c991ad4a0dfa083fca88a463d0
Benson Liu <bensonhliu@gmail.com>
2022-08-08 22:10:13 -0700

    implemented Prettier linting checks and git pre-commit hooks with Husky

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,25 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div>
       <Head>
         <title>LA CTF Home</title>
-        <meta name="description" content="LA CTF is a jeopardy-style capture-the-flag (CTF) style cybersecurity competition hosted ACM Cyber @ UCLA & Psi Beta Rho!" />
+        <meta
+          name="description"
+          content="LA CTF is a jeopardy-style capture-the-flag (CTF) style cybersecurity competition hosted ACM Cyber @ UCLA & Psi Beta Rho!"
+        />
         <link rel="icon" href="./favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1>Welcome to LA CTF!</h1>
-        <p>LA CTF is a jeopardy-style capture-the-flag (CTF) style cybersecurity competition hosted ACM Cyber @ UCLA & Psi Beta Rho!</p>
+        <p>
+          LA CTF is a jeopardy-style capture-the-flag (CTF) style cybersecurity
+          competition hosted ACM Cyber @ UCLA & Psi Beta Rho!
+        </p>
 
         <h2>Join Us</h2>
 
@@ -26,5 +32,5 @@ export default function Home() {
         <p>© ACM Cyber at UCLA 2022</p>
       </footer>
     </div>
-  )
+  );
 }
