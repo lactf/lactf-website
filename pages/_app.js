601fe395274716afc62833bb9bb7d28281a643f7
Benson Liu <bensonhliu@gmail.com>
2022-08-22 07:42:41 -0700

    implemented basic NextSeo structure and added title and description

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import { NextSeo } from "next-seo";
 
 import "../styles/globals.css";
 
@@ -6,10 +7,22 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>LA CTF | Home</title>
-        <meta
-          name="description"
-          content="LA CTF is a jeopardy-style capture-the-flag (CTF) cybersecurity competition hosted by ACM Cyber at UCLA & Psi Beta Rho!"
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <NextSeo
+          title="Home | LA CTF"
+          description="LA CTF is a jeopardy-style capture-the-flag (CTF) cybersecurity competition hosted by ACM Cyber at UCLA & Psi Beta Rho!"
+          openGraph={{
+            images: [
+              {
+                url: "https://www.uclaacm.com/images/logo.png",
+                width: 1200,
+                height: 1200,
+                alt: "LA CTF logo",
+              },
+            ],
+            site_name: "LA CTF",
+          }}
         />
         <link rel="icon" href="./favicon.ico" />
       </Head>
