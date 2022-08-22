78c6e04383e8edf8d483cbefe6cb0fd4eb930795
Juliet Zhang <julietzhang3@gmail.com>
2022-08-22 00:18:37 -0700

    Add About, Prizes, and Contacts sections

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
 import Image from "next/image";
-
 import Navbar from "../components/Navbar";
 
 import Flag from "../public/images/flag.png";
+import CyberGM from "../public/images/cyberGM.png";
+import Discord from "../public/images/discord.png";
+import Email from "../public/images/email.png";
+import Instagram from "../public/images/instagram.png";
 
 import styles from "../styles/Home.module.css";
 
@@ -15,42 +18,67 @@ export default function Home() {
         <p className={styles.subheading}>
           February, 10th 8:00 pm PST - February 12th 2:00 pm PST (42 hours)
         </p>
+      </main>
 
-        <h2 id="about">
+      <div className={styles.aboutContainer}>
+        <div className={styles.aboutPic}>
           <Image
-            src={Flag}
-            width={60}
-            height={60}
-            className={styles.flag}
-            alt="Pink LA CTF flag."
+            src={CyberGM}
+            style={{ borderRadius: "8px" }}
+            alt="Cyber General Meeting."
           />
-          <span className={styles.headerText}>About</span>
-        </h2>
-        <p>
-          LA CTF is an annual Capture the Flag (CTF) cybersecurity competition
-          hosted by ACM Cyper at UCLA. LA CTF is open to all skill levels of
-          cybersecurity! Whether you are tackling your first exploit or have
-          professional experience, there will be challenges just right for you!
-          There will be a variety of events ranging from the competition
-          containing jeopardy-style cybersecurity challenges to talks from UCLA
-          professors to fun events such as typing competitions! If you are
-          interested in attending, join the Discord to stay up to date with the
-          latest information about LA CTF!
-        </p>
+        </div>
+        <div className={styles.aboutText}>
+          <p>
+            LA CTF is an annual Capture the Flag (CTF) cybersecurity competition
+            hosted by ACM Cyber at UCLA. LA CTF is open to all skill levels of
+            cybersecurity! Whether you are tackling your first exploit or have
+            professional experience, there will be challenges just right for
+            you! There will be a variety of events ranging from the competition
+            containing jeopardy-style cybersecurity challenges to talks from
+            UCLA professors to fun events such as typing competitions! If you
+            are interested in attending, join the Discord to stay up to date
+            with the latest information about LA CTF!
+          </p>
+        </div>
+      </div>
 
-        <h2 id="prizes">
-          <Image src={Flag} width={60} height={60} alt="Pink LA CTF flag." />
-          <span className={styles.headerText}>Prizes</span>
-        </h2>
-        <p>
-          LA CTF will have prizes for both the beginner and open sections! More
-          information to come.
-        </p>
+      <main className={styles.main}>
+        <div className={styles.twoColumns}>
+          <div className={styles.prizes}>
+            <Image src={Flag} width={60} height={60} alt="Pink LA CTF flag." />
+            <span className={styles.headerText}>Prizes</span>
+            <p>
+              LA CTF will have prizes for both the beginner and open sections!
+              More information to come.
+            </p>
+          </div>
 
-        <h2 id="contacts">
-          <Image src={Flag} width={60} height={60} alt="Pink LA CTF flag." />
-          <span className={styles.headerText}>Contacts</span>
-        </h2>
+          <div className={styles.contacts}>
+            <Image src={Flag} width={60} height={60} alt="Pink LA CTF flag." />
+            <span className={styles.headerText}>Contacts</span>
+            <br />
+            <a href="https://discord.gg/VEJf6gqdP5">
+              <Image src={Discord} height={25} width={30} alt="Discord logo." />
+              <span className={styles.contactsLink}>discord.gg/VEJf6gqdP5</span>
+            </a>
+            <br />
+            <a href="mailto:uclacyber@gmail.com">
+              <Image src={Email} height={30} width={30} alt="Email symbol." />
+              <span className={styles.contactsLink}>uclacyber@gmail.com</span>
+            </a>
+            <br />
+            <a href="https://www.instagram.com/uclacyber">
+              <Image
+                src={Instagram}
+                height={30}
+                width={30}
+                alt="Instagram logo."
+              />
+              <span className={styles.contactsLink}>@uclacyber</span>
+            </a>
+          </div>
+        </div>
 
         <h2 id="speakers">
           <Image src={Flag} width={60} height={60} alt="Pink LA CTF flag." />
