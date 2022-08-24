a682799854203bdb2425a242c5e73b40040067f0
Benson Liu <bensonhliu@gmail.com>
2022-08-23 22:49:22 -0700

    CI improvements * implemented CI ENV to allow for assets to be properly deployed * added pinst for husky installation errors * updated GitHub action

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
+const debug = process.env.NODE_ENV !== "production";
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
     loader: 'akamai',
     path: '',
   },
-  assetPrefix: '',
+  assetPrefix: !debug ? 'https://uclaacm.github.io/lactf-website/' : '',
 }
 
 module.exports = nextConfig
