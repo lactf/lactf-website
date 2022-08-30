fa4d8e94ffc01d7c2db05358569905f1bba497ca
Benson Liu <bensonhliu@gmail.com>
2022-08-29 23:30:46 -0700

    attempt to fix console warnings from overhydration

--- components/Countdown.js
+++ components/Countdown.js
@@ -1,11 +1,12 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 import styles from "../styles/Countdown.module.css";
 
 const Countdown = () => {
-  let [time, setTime] = useState(1676088000 - Date.now() / 1000);
+  let [time, setTime] = useState(0);
 
   useEffect(() => {
+    if (time === 0) setTime(1676088000 - Date.now() / 1000);
     setInterval(() => setTime(1676088000 - Date.now() / 1000), 5000);
   }, [time]);
 
