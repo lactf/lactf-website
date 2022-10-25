61c764747e53d478882cf928b3884cd0799766f9
Alec <31748545+burturt@users.noreply.github.com>
2022-10-25 01:07:15 -0700

    Update about to new figma design (#32)
    * Fix horizonal scrollbar on desktop sizes
    
    * Run Prettier on code
    
    * Change about section to new format
    
    * Add drop shadow and remove leftover content
    
    * Add top padding to images after image height shrunk
    
    * ran prettier
    
    Co-authored-by: Benson Liu <bensonhliu@gmail.com>
    Co-authored-by: alec <31748545+burturt@users.noreply.github.com>

--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,11 @@ import Instagram from "../public/images/instagram.png";
 import LongLogo from "../public/images/LongLogoWaving.gif";
 import WavingFlag from "../public/images/TransparentWavingFlag.gif";
 
+import aboutEvents from "../public/images/aboutEvents.png";
+import aboutHosts from "../public/images/aboutHosts.png";
+import aboutSpeakers from "../public/images/aboutSpeakers.png";
+import aboutLevels from "../public/images/aboutLevels.png";
+
 import styles from "../styles/Home.module.css";
 import Carousel from "../components/Carousel";
 
@@ -38,47 +43,67 @@ export default function Home() {
         }}
       />
       <Navbar />
-      <main className={styles.main}>
-        <div className={styles.logoContainer}>
-          <Image src={LongLogo} alt="Pink LA CTF flag." />
-        </div>
-        <p className={styles.subheading}>
-          February 10th, 2023 (8:00 pm PST) - February 12th, 2023 (2:00 pm PST)
-        </p>
-        <Countdown />
-
-        <div className={styles.aboutContainer} id="about">
-          <div className={styles.aboutText}>
-            <h2>About Us</h2>
-            <p>
-              LA CTF is an annual Capture the Flag (CTF) cybersecurity
-              competition hosted by ACM Cyber at UCLA. LA CTF is open to all
-              skill levels of cybersecurity! Whether you are tackling your first
-              exploit or have professional experience, there will be challenges
-              just right for you! There will be a variety of events ranging from
-              the competition containing jeopardy-style cybersecurity challenges
-              to talks from UCLA professors to fun events such as typing
-              competitions! If you are interested in attending, join the Discord
-              to stay up to date with the latest information about LA CTF!
-            </p>
+      <main>
+        <section className={styles.main}>
+          <div className={styles.logoContainer}>
+            <Image src={LongLogo} alt="Pink LA CTF flag." />
           </div>
-        </div>
-
-        <div className={styles.prizes} id="prizes">
-          <Image
-            src={WavingFlag}
-            width={50}
-            height={60}
-            alt="Waving pink LA CTF flag."
-          />
-          <span className={styles.headerText}>Prizes</span>
-          <p>
-            LA CTF will have prizes for both the beginner and open sections!
-            More information to come.
+          <p className={styles.subheading}>
+            February 10th, 2023 (8:00 pm PST) - February 12th, 2023 (2:00 pm
+            PST)
           </p>
-        </div>
+          <Countdown />
+        </section>
+        <section className={styles.aboutContainer} id="about">
+          <div className={styles.aboutFlexContainer}>
+            <div className={styles.aboutBlock}>
+              <div className={styles.aboutImageContainer}>
+                <Image className={styles.aboutImage} src={aboutHosts}></Image>
+              </div>
+              <h2>Hosts</h2>
+              <p>
+                LA CTF is an annual cybersecurity competition hosted by ACM
+                Cyber at UCLA
+              </p>
+            </div>
+            <div className={styles.aboutBlock}>
+              <div className={styles.aboutImageContainer}>
+                <Image className={styles.aboutImage} src={aboutEvents}></Image>
+              </div>
+              <h2>Events</h2>
+              <p>
+                There will be a variety of events ranging from competitive
+                cybersecurity challenges to more relaxed events like typing
+                competitions
+              </p>
+            </div>
+            <div className={styles.aboutBlock}>
+              <div className={styles.aboutImageContainer}>
+                <Image
+                  className={styles.aboutImage}
+                  src={aboutSpeakers}
+                ></Image>
+              </div>
+              <h2>Speakers</h2>
+              <p>
+                Throughout the event, a variety of UCLA professors and
+                cybersecurity experts will be giving talks
+              </p>
+            </div>
+            <div className={styles.aboutBlock}>
+              <div className={styles.aboutImageContainer}>
+                <Image className={styles.aboutImage} src={aboutLevels}></Image>
+              </div>
+              <h2>Levels</h2>
+              <p>
+                LA CTF is open to all skill levels! No matter yhour experience,
+                there will be challenges just right for you!
+              </p>
+            </div>
+          </div>
+        </section>
 
-        <div className={styles.twoColumns}>
+        <section className={styles.main}>
           <div className={styles.prizes} id="prizes">
             <Image
               src={WavingFlag}
@@ -86,125 +111,147 @@ export default function Home() {
               height={60}
               alt="Waving pink LA CTF flag."
             />
-            <span className={styles.headerText}>Beginner Section</span>
+            <span className={styles.headerText}>Prizes</span>
             <p>
-              The Beginner Section will be composed of teams of at most four
-              students. The challenges are designed to be easier and provide
-              more learning opportunities for beginners to CTFs. While members
-              of the team can be from anywhere, only UCLA teams will be awarded
-              prizes for winning in the Beginner Section.
+              LA CTF will have prizes for both the beginner and open sections!
+              More information to come.
             </p>
           </div>
 
-          <div className={styles.contacts} id="contact">
+          <div className={styles.twoColumns}>
+            <div className={styles.prizes} id="prizes">
+              <Image
+                src={WavingFlag}
+                width={50}
+                height={60}
+                alt="Waving pink LA CTF flag."
+              />
+              <span className={styles.headerText}>Beginner Section</span>
+              <p>
+                The Beginner Section will be composed of teams of at most four
+                students. The challenges are designed to be easier and provide
+                more learning opportunities for beginners to CTFs. While members
+                of the team can be from anywhere, only UCLA teams will be
+                awarded prizes for winning in the Beginner Section.
+              </p>
+            </div>
+
+            <div className={styles.contacts} id="contact">
+              <Image
+                src={WavingFlag}
+                width={50}
+                height={60}
+                alt="Waving pink LA CTF flag."
+              />
+              <span className={styles.headerText}>Open Section</span>
+              <p>
+                The Open Section will be composed of teams with no size limit.
+                The challenges will range from intermediate to advanced
+                difficulty. All of the challenges made available for the
+                Beginner Section will be made available for the option section.
+                Teams in the option section may consist of members from
+                anywhere.
+              </p>
+            </div>
+          </div>
+
+          <h2 id="speakers">
             <Image
               src={WavingFlag}
               width={50}
               height={60}
               alt="Waving pink LA CTF flag."
             />
-            <span className={styles.headerText}>Open Section</span>
-            <p>
-              The Open Section will be composed of teams with no size limit. The
-              challenges will range from intermediate to advanced difficulty.
-              All of the challenges made available for the Beginner Section will
-              be made available for the option section. Teams in the option
-              section may consist of members from anywhere.
-            </p>
+            <span className={styles.headerText}>Speakers</span>
+          </h2>
+          <p>
+            Here are some of the exciting speakers that will be attending LA
+            CTF!
+          </p>
+
+          <Carousel />
+
+          <div className={styles.twoColumns}>
+            <div className={styles.groupPic}>
+              <Image
+                src={CyberGM}
+                style={{ borderRadius: "8px" }}
+                alt="Cyber General Meeting."
+              />
+            </div>
+
+            <div className={styles.contacts} id="contact">
+              <Image
+                src={WavingFlag}
+                width={50}
+                height={60}
+                alt="Waving pink LA CTF flag."
+              />
+              <span className={styles.headerText}>Contacts</span>
+              <ul>
+                <li>
+                  <Image
+                    src={Discord}
+                    height={45}
+                    width={55}
+                    alt="Discord logo."
+                  />
+                  <a
+                    href="https://discord.gg/VEJf6gqdP5"
+                    className={`${styles.contactsLink} ${styles.discord}`}
+                  >
+                    discord.gg/VEJf6gqdP5
+                  </a>
+                </li>
+                <li>
+                  <Image
+                    src={Email}
+                    height={50}
+                    width={50}
+                    alt="Email symbol."
+                  />
+                  <a
+                    href="mailto:uclacyber@gmail.com"
+                    className={styles.contactsLink}
+                  >
+                    uclacyber@gmail.com
+                  </a>
+                </li>
+                <li>
+                  <Image
+                    src={Instagram}
+                    height={50}
+                    width={50}
+                    alt="Instagram logo."
+                  />
+                  <a
+                    href="https://www.instagram.com/uclacyber"
+                    className={styles.contactsLink}
+                  >
+                    @uclacyber
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
-        </div>
 
-        <h2 id="speakers">
-          <Image
-            src={WavingFlag}
-            width={50}
-            height={60}
-            alt="Waving pink LA CTF flag."
-          />
-          <span className={styles.headerText}>Speakers</span>
-        </h2>
-        <p>
-          Here are some of the exciting speakers that will be attending LA CTF!
-        </p>
-
-        <Carousel />
-
-        <div className={styles.twoColumns}>
-          <div className={styles.groupPic}>
-            <Image
-              src={CyberGM}
-              style={{ borderRadius: "8px" }}
-              alt="Cyber General Meeting."
-            />
-          </div>
-
-          <div className={styles.contacts} id="contact">
+          <h2 id="sponsors">
             <Image
               src={WavingFlag}
               width={50}
               height={60}
               alt="Waving pink LA CTF flag."
             />
-            <span className={styles.headerText}>Contacts</span>
-            <ul>
-              <li>
-                <Image
-                  src={Discord}
-                  height={45}
-                  width={55}
-                  alt="Discord logo."
-                />
-                <a
-                  href="https://discord.gg/VEJf6gqdP5"
-                  className={`${styles.contactsLink} ${styles.discord}`}
-                >
-                  discord.gg/VEJf6gqdP5
-                </a>
-              </li>
-              <li>
-                <Image src={Email} height={50} width={50} alt="Email symbol." />
-                <a
-                  href="mailto:uclacyber@gmail.com"
-                  className={styles.contactsLink}
-                >
-                  uclacyber@gmail.com
-                </a>
-              </li>
-              <li>
-                <Image
-                  src={Instagram}
-                  height={50}
-                  width={50}
-                  alt="Instagram logo."
-                />
-                <a
-                  href="https://www.instagram.com/uclacyber"
-                  className={styles.contactsLink}
-                >
-                  @uclacyber
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-
-        <h2 id="sponsors">
-          <Image
-            src={WavingFlag}
-            width={50}
-            height={60}
-            alt="Waving pink LA CTF flag."
-          />
-          <span className={styles.headerText}>Sponsors</span>
-        </h2>
-        <p>
-          LA CTF would not be possible without all of these amazing sponsors!
-          Want your company to be a sponsor? Reach out to{" "}
-          <a href="mailto:uclacyber@gmail.com" className={styles.mailto}>
-            uclacyber@gmail.com
-          </a>
-          .
-        </p>
+            <span className={styles.headerText}>Sponsors</span>
+          </h2>
+          <p>
+            LA CTF would not be possible without all of these amazing sponsors!
+            Want your company to be a sponsor? Reach out to{" "}
+            <a href="mailto:uclacyber@gmail.com" className={styles.mailto}>
+              uclacyber@gmail.com
+            </a>
+          </p>
+        </section>
       </main>
 
       <Footer />
