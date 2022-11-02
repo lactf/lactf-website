7aeccbc7b30ff835d54abfdaf062579adeb5999f
alec <31748545+burturt@users.noreply.github.com>
2022-11-01 22:07:55 -0700

    fix redirect since github pages is actually static

---
+++ pages/discord.js
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import Footer from "../components/Footer";
+
+import Navbar from "../components/Navbar";
+
+import styles from "../styles/Discord.module.css";
+
+import Script from "next/script";
+
+export default function Discord() {
+  return (
+    <div>
+      <Navbar />
+      <main className={styles.redirect}>
+        <h1>Redirecting you now...</h1>
+        <p className={styles.home}>
+          <Link href="https://discord.gg/uZM6vxqHDq">
+            Click here if you are not redirected automatically
+          </Link>
+        </p>
+      </main>
+      <Script>window.location.href = "https://discord.gg/uZM6vxqHDq";</Script>
+      <Footer />
+    </div>
+  );
+}
