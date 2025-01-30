0031b5d0fca84245d827a1cd7c71b61987eaec91
Teong Seng Tan <tanteongseng2@gmail.com>
2025-01-30 01:38:57 -0800

    Edited padding for LA CTF Mini section

--- components/LACTFMiniStrip.js
+++ components/LACTFMiniStrip.js
@@ -4,11 +4,12 @@ import foodSvg from "../public/images/lactfmini-food.svg";
 import tshirtSvg from "../public/images/lactfmini-tshirt.svg";
 import hangoutSvg from "../public/images/lactfmini-hangout.svg";
 
-import styles from "../styles/AboutStrip.module.css";
+import styles from "../styles/LACTFMini.module.css";
+import styles_about from "../styles/AboutStrip.module.css";
 
 const AboutStrip = () => {
   return (
-    <div className={styles.aboutFlexContainer}>
+    <div className={styles.lactfminiFlexContainer}>
       <AboutCard icon={foodSvg} big={true} title="Food" />
       <AboutCard icon={tshirtSvg} big={true} title="Swag" />
       <AboutCard
@@ -22,10 +23,10 @@ const AboutStrip = () => {
 
 const AboutCard = ({ icon, big, title, desc }) => {
   return (
-    <div className={styles.aboutBlock}>
-      <div className={styles.aboutImageContainer}>
+    <div className={styles_about.aboutBlock}>
+      <div className={styles_about.aboutImageContainer}>
         <Image
-          className={big ? styles.aboutImageBig : styles.aboutImageSmall}
+          className={big ? styles_about.aboutImageBig : styles_about.aboutImageSmall}
           src={icon}
           layout="fill"
           alt="About icon showing details about LA CTF."
