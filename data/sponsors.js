e8e2f4ab2965e1317a39c2b1e1fda660da7ff366
unknown <katiemin24@gmail.com>
2025-11-21 23:27:08 -0800

    add google cloud

--- data/sponsors.js
+++ data/sponsors.js
@@ -1,14 +1,11 @@
 import googlecloud from "../public/images/google_cloud.png";
 import latticework from "../public/images/latticework.png";
 import ottersec from "../public/images/ottersec.png";
-import sandia from "../public/images/sandia_national_laboratories.svg";
 import trailofbits from "../public/images/trail-of-bits.png";
 import affiliateFlag from "../public/images/affiliateFlag.svg";
 import infraFlag from "../public/images/infraFlag.svg";
 import lockheedmartin from "../public/images/lockheed_martin.svg";
 import tryhackme from "../public/images/tryhackme.png";
-import crowdstrike from "../public/images/crowdstrike.png";
-import spacex from "../public/images/spacex.png";
 import binaryNinja from "../public/images/binary-ninja.png";
 import microsoft from "../public/images/microsoft.jpg";
 import aerospace from "../public/images/aerospace.png";
@@ -16,54 +13,6 @@ import lockheed from "../public/images/lockheed.png";
 import verkada from "../public/images/verkada.png";
 import zellic from "../public/images/zellic.png";
 
-// const data = {
-//   sponsors: [
-//     {
-//       name: "Sandia National Laboratories",
-//       url: "https://www.sandia.gov/",
-//       image: sandia,
-//       flag: {
-//         image: affiliateFlag,
-//         blurb: "Affiliate",
-//       },
-//       // Taken from https://www.sandia.gov/missions/
-//       blurb:
-//         "Sandia strives to become the laboratory that the U.S. turns to first for technology solutions to the most challenging problems that threaten peace and freedom for our nation and the globe.",
-//     },
-//     {
-//       name: "Google Cloud",
-//       url: "https://goo.gle/ctfsponsorship",
-//       image: googlecloud,
-//       blurb:
-//         "Meet your business challenges head on with cloud computing services from Google, including data management, hybrid & multi-cloud, and AI & ML.",
-//       flag: {
-//         image: infraFlag,
-//         blurb: "Infra sponsored by goo.gle/ctfsponsorship",
-//       },
-//     },
-//     {
-//       name: "Trail of Bits",
-//       url: "https://www.trailofbits.com/",
-//       image: trailofbits,
-//       blurb:
-//         "Trail of Bits helps secure the world's most targeted organizations and products. We combine high-end security research with a real-world attacker mentality to reduce risk and fortify code.",
-//     },
-//     {
-//       name: "OtterSec",
-//       url: "https://osec.io/",
-//       image: ottersec,
-//       blurb:
-//         "OtterSec is the leading blockchain security audit team, working closely with various Layer 1s and the protocols built on them.",
-//     },
-//     {
-//       name: "Amber by LatticeWork",
-//       url: "https://www.myamberlife.com/",
-//       image: latticework,
-//       blurb:
-//         "Store your data locally and access it from anywhere without subscription fees! UCLA students, open a ticket in the LA CTF discord for a 25% discount.",
-//     },
-//   ],
-// };
 const data = {
   sponsors: [
     // {
@@ -104,17 +53,6 @@ const data = {
     //   // Taken from https://www.microsoft.com/en-us/about
     //   blurb:
     //     "Microsoft's mission is to empower every person and every organization on the planet to achieve more.",
-    // },
-    // {
-    //   name: "Google Cloud",
-    //   url: "https://goo.gle/ctfsponsorship",
-    //   image: googlecloud,
-    //   blurb:
-    //     "Meet your business challenges head on with cloud computing services from Google, including data management, hybrid & multi-cloud, and AI & ML.",
-    //   flag: {
-    //     image: infraFlag,
-    //     blurb: "Infra sponsored by goo.gle/ctfsponsorship",
-    //   },
     // },
     // {
     //   name: "CrowdStrike",
@@ -165,7 +103,18 @@ const data = {
       image: zellic,
       blurb:
         "Zellic is a security research firm. We hire talented hackers to break critical software, ranging from virtual machines to compilers and major web apps to proof systems. Before Zellic, we founded perfect blue, the #1 CTF team in 2020 and 2021. We offer a comprehensive benefits package and direct equity participation, as well as flexible hours, remote work, and both full-time and part-time roles. To learn more about Zellic, check out our blog: zellic.io/auditooor-grindset. Work at Zellic: jobs@zellic.io | @gf_256",
-    }
+    },
+    {
+      name: "Google Cloud",
+      url: "https://goo.gle/ctfsponsorship",
+      image: googlecloud,
+      blurb:
+        "Meet your business challenges head on with cloud computing services from Google, including data management, hybrid & multi-cloud, and AI & ML.",
+      flag: {
+        image: infraFlag,
+        blurb: "Infra sponsored by goo.gle/ctfsponsorship",
+      },
+    },
   ],
 };
 
