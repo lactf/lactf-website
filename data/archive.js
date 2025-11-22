7b20d22eb44ce4a0023ff832d740af15e6efaee4
unknown <katiemin24@gmail.com>
2025-11-21 23:38:04 -0800

    add 2025 to archive

--- data/archive.js
+++ data/archive.js
@@ -1,6 +1,12 @@
 const data = {
     entries: [
         {
+            year: 2025,
+            website: "https://website.2025.lac.tf",
+            platform: "https://platform.2025.lac.tf",
+            github: "https://github.com/uclaacm/lactf-archive/tree/main/2025"
+        },
+        {
             year: 2024,
             website: "https://website.2024.lac.tf",
             platform: "https://platform.2024.lac.tf",
