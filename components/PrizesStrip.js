b5778318029f95436b54220f5b05e4f61cdc085a
unknown <katiemin24@gmail.com>
2026-01-18 22:22:28 -0800

    update prizes and speakers

--- components/PrizesStrip.js
+++ components/PrizesStrip.js
@@ -13,47 +13,77 @@ const PrizesStrip = () => {
           </p>
           <br />
           <br />
-          {/*
+          {
             <ul className={styles.prizelist}>
               <li>
-                1st place: $300 +<br />
+                1st place: $240 + <br />
+                <a
+                  className={styles.prizelink}
+                  href="https://www.offsec.com/products/90-day-bundle/"
+                >
+                  1x OffSec Course and Cert Bundle
+                </a>{" "}
+                + <br />
                 <a
                   className={styles.prizelink}
                   href="https://www.latticeworkinc.com/hardware/amberpro/"
                 >
-                  1x Amber Pro Smart Storage Device
+                  3x Amber Pro Smart Storage Device
                 </a>{" "}
                 + <br />
                 <a
                   className={styles.prizelink}
                   href="https://binary.ninja/features/"
                 >
-                  2x Binary Ninja Licenses
+                  1x Binary Ninja license
+                </a>{" "}
+                + <br />
+                <a
+                  className={styles.prizelink}
+                  href="https://spearphish-general-store.myshopify.com/"
+                >
+                  1x $50 Spearphish Store gift card
                 </a>{" "}
               </li>
               <li>
-                2nd place: $240 +<br />
-                <a
-                  className={styles.prizelink}
-                  href="https://www.latticeworkinc.com/hardware/amberpro/"
-                >
-                  1x Amber Pro Smart Storage Device
-                </a>{" "}
-                + <br />
+                2nd place: $200 + <br />
                 <a
                   className={styles.prizelink}
                   href="https://binary.ninja/features/"
                 >
-                  2x Binary Ninja Licenses
+                  1x Binary Ninja license
+                </a>{" "}
+                + <br />
+                <a
+                  className={styles.prizelink}
+                  href="https://spearphish-general-store.myshopify.com/"
+                >
+                  1x $50 Spearphish Store gift card
                 </a>{" "}
               </li>
-              <li>3rd place: $200</li>
-              <li>4th & 5th place: $130</li>
+              <li>
+                3rd place: $160 + <br />
+                <a
+                  className={styles.prizelink}
+                  href="https://spearphish-general-store.myshopify.com/"
+                >
+                  1x $50 Spearphish Store gift card
+                </a>{" "}
+              </li>
+              <li>
+                4th & 5th place: $80 + <br />
+                <a
+                  className={styles.prizelink}
+                  href="https://spearphish-general-store.myshopify.com/"
+                >
+                  1x $50 Spearphish Store gift card
+                </a>{" "}
+              </li>
               <li>6th-10th place: $40</li>
             </ul>
-            */}
+          }
           <br />
-          {/*
+          {
             <p>
               UCLA division prizes will be distributed directly to individual
               team members in the form of{" "}
@@ -62,7 +92,7 @@ const PrizesStrip = () => {
               </a>
               . Amounts listed are per team.
             </p>
-            */}
+          }
           <br />
         </div>
         <div className={styles.prizesBlock}>
@@ -74,36 +104,61 @@ const PrizesStrip = () => {
           </p>
           <br />
           <br />
-          {/*
+          {
             <ul className={styles.prizelist}>
               <li>
-                1st place: $650 + <br />
+                1st place: $500 + <br />
                 <a
                   className={styles.prizelink}
-                  href="https://binary.ninja/features/"
+                  href="https://hex-rays.com/pricing"
                 >
-                  2× Binary Ninja Licenses
+                  3× IDA Pro Expert 2 license
                 </a>{" "}
                 + <br />
-                <a className={styles.prizelink} href="https://ctf.dicega.ng/">
-                  DiceCTF 2025 Finals invitation
-                </a>
-              </li>
-              <li>
-                2nd place: $400 + <br />
                 <a
                   className={styles.prizelink}
                   href="https://binary.ninja/features/"
                 >
-                  1× Binary Ninja License
+                  1× Binary Ninja license
                 </a>{" "}
               </li>
-              <li>3rd place: $200</li>
+              <li>
+                2nd place: $300 + <br />
+                <a
+                  className={styles.prizelink}
+                  href="https://hex-rays.com/pricing"
+                >
+                  1× IDA Pro Expert 2 license
+                </a>{" "}
+                + <br />
+                <a
+                  className={styles.prizelink}
+                  href="https://binary.ninja/features/"
+                >
+                  1× Binary Ninja license
+                </a>{" "}
+              </li>
+              <li>
+                3rd place: $200 + <br />
+                <a
+                  className={styles.prizelink}
+                  href="https://hex-rays.com/pricing"
+                >
+                  1× IDA Pro Expert 2 license
+                </a>{" "}
+                + <br />
+                <a
+                  className={styles.prizelink}
+                  href="https://binary.ninja/features/"
+                >
+                  1× Binary Ninja license
+                </a>{" "}
+              </li>
               <li>4th & 5th place: $100</li>
             </ul>
-            */}
+          }
           <br />
-          {/*
+          {
             <p>
               Open division prizes will be distributed to the email linked to
               the given winning teams in the form of{" "}
@@ -112,29 +167,29 @@ const PrizesStrip = () => {
               </a>
               . Amounts listed are per team.
             </p>
-            */}
+          }
           <br />
         </div>
       </div>
-      {/*
+      {
         <p className={styles.prizebetween}>
           Additionally,{" "}
           <a className={styles.prizelink} href="https://binary.ninja/features/">
-            1 Binary Ninja License
+            1x Binary Ninja license
           </a>{" "}
           will be awarded for the best Binary Ninja writeup (either division).{" "}
           <br />
           Prizes are subject to change and may be made conditional on proof of
           challenge completion (writeups).
         </p>
-        */}
+      }
       <div className={styles.writeprizecontainer}>
         <div className={styles.writeupprizelist}>
-          <h3>
+          {/*<h3>
             Prizes, including writeup prizes, will be announced closer to the
             competition date. Stay tuned!
-          </h3>
-          {/* <h3>Writeup Prizes:</h3>
+          </h3>*/}
+          <h3>Writeup Prizes:</h3>
           <br />
           <ul>
             <li>
@@ -153,7 +208,7 @@ const PrizesStrip = () => {
               The writeup submission form will be active for 1 week after the
               CTF is over, and will be posted in our discord.
             </li>
-          </ul> */}
+          </ul>
         </div>
       </div>
     </div>
