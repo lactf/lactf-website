38ea8213c8584dd448e335424ea0be5dcf9304c2
unknown <katiemin24@gmail.com>
2026-01-20 17:21:39 -0800

    add speaker

--- data/speakers.js
+++ data/speakers.js
@@ -1,4 +1,5 @@
 import nader from "../public/images/nader.jpg";
+import yuan from "../public/images/ytian.webp";
 import alyssa from "../public/images/alyssa.jpeg";
 import akshara from "../public/images/akshara.png";
 import sanjana from "../public/images/sanjana.jpg";
@@ -15,6 +16,12 @@ const data = {
       title: "Assistant Professor, UCLA Electrical and Computer Engineering Department",
       info: "Nader Sehatbakhsh runs the Secure Systems and Architectures (SsysArch) research lab at UCLA. His research interest is in the broad area of Security and Privacy with emphasis on hardware support for security and privacy. Specifically, he is interested in computer architecture, IoT Security, embedded/cyber-physical systems, and security and privacy in machine learning. He is affiliated with the Computer Engineering Lab in Electrical and Computer Engineering (ECE) Department at UCLA. Prior to joining to UCLA, Dr. Nader Sehatbakhsh was a PhD student at the School of Computer Science at Georgia Tech where he worked on Securing Computing Systems by Mitigating and Leveraging Side-Channels. He earned his PhD in Computer Science from Georgia Tech in 2020. Before that, he earned his B.Sc. in Electrical Engineering from the University of Tehran in 2013. He joined UCLA since July 2020.",
       image: nader,
+    },
+    {
+      name: "Dr. Yuan Tian",
+      title: "Assistant Professor, UCLA Electrical and Computer Engineering Department",
+      info: "Yuan Tian is an Associate Professor of Electrical and Computer Engineering, Computer Science, and the Institute for Technology, Law and Policy (ITLP) at the University of California, Los Angeles. Her research interests involve security and privacy and their interactions with computer systems, machine learning, and human-computer interaction. Her current research focuses on developing new computing platforms with strong security and privacy features, particularly in the Generative AI and Internet of Things. Her work has real-world impacts as countermeasures and design changes have been integrated into platforms (such as Android, Chrome, Azure, and iOS) and also impacted the security recommendations of standard organizations such as the Internet Engineering Task Force (IETF). She is a recipient of the Okawa Fundation Award 2022, Google Research Scholar Award 2021, Facebook Research Award 2021, NSF CAREER award 2020, NSF CRII award 2019, Amazon AI Faculty Fellowship 2019. Her research has appeared in top-tier venues in security, machine learning, and systems. Her projects have been covered by media outlets such as IEEE Spectrum, Forbes, Fortune, Wired, and Telegraph.",
+      image: yuan,
     },
     // {
     //   name: "Akshara Sundararajan",
