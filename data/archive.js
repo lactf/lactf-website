5ffc865e6d96dcfe1b6e0da26a9fa2ea9ac49e34
Alec <31748545+burturt@users.noreply.github.com>
2026-02-17 23:42:09 -0800

    add 2026 to archive

--- data/archive.js
+++ data/archive.js
@@ -1,6 +1,12 @@
 const data = {
     entries: [
         {
+            year: 2026,
+            website: "https://website.2026.lac.tf",
+            platform: "https://platform.2026.lac.tf",
+            github: "https://github.com/uclaacm/lactf-archive/tree/main/2026"
+        },
+        {
             year: 2025,
             website: "https://website.2025.lac.tf",
             platform: "https://platform.2025.lac.tf",
